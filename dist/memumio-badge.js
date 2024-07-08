2e06de81445ba9f9d9a0f120a84b0b381bd9a407
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-07-08 00:03:58 -0400

    Initial commit

---
+++ dist/memumio-badge.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Memum Badge Fixer
+// @namespace    http://tampermonkey.net
+// @version      1.0
+// @description  Fix memum badges.
+// @author       Ernie Serrano
+// @match        https://www.memum.io/player/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=memum.io
+// @grant        none
+// ==/UserScript==
+let fixBadgesInterval = null;
+function fixBadges() {
+  for (const badge of document.getElementsByClassName("badge")) {
+    const newHTML = badge.innerHTML.replace("Moderator", "Manger");
+    badge.innerHTML = newHTML;
+    if (fixBadgesInterval !== null) {
+      clearInterval(fixBadgesInterval);
+    }
+  }
+}
+setInterval(fixBadges, 250);
